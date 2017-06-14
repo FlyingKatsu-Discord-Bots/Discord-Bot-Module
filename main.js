225b6bd378697e4b2929cff6a287eb650a2f1c2a
Flying Katsu <Warped2713@users.noreply.github.com>
2017-06-13 23:20:20 -0400

    filled in main.js to use our modules with customization

--- main.js
+++ main.js
@@ -1,7 +1,36 @@
-// TODO: add includes here
+// Get the module stuff we need
+const DiscordBot = require('./sub/DiscordBot.js');
+// Get customizations for this bot
+const BotHandler = require('./custom/CustomBotHandler.js');
+// TODO: add links for custom error, util, commands, http response, db
 
-module.exports = {
-  
-  // TODO: add interface stuff here
-  
-}
+// Define/Load config data here
+const BotConfig = require('./config/bot.json');
+const MasterConfig = require('./config/master.json');
+const GuildConfig = require('./config/guild.json');
+const WebhookConfig = require('./config/webhook.json');
+const DatabaseConfig = require('./config/database.json');
+
+
+// Create the bot
+const ExampleBot = new DiscordBot( BotConfig );
+
+
+// Add master config to control who can use this bot
+ExampleBot.initMaster( MasterConfig );
+// Add guild config for defining what the bot can do in each server
+ExampleBot.initGuild( GuildConfig );
+
+
+// [OPTIONAL] Add webhook config for HTTP/S server listening
+ExampleBot.initServer( WebhookConfig );
+// [OPTIONAL] Add database config for preserving user data
+ExampleBot.initDatabase( DatabaseConfig );
+
+
+// Set up the Discord.js event handlers
+ExampleBot.initBotHandlers( new BotHandler(ExampleBot) );
+// TODO: add inits for custom error, util, commands, http response, db
+
+// Login to Discord, after starting up the server and/or database if initialized
+ExampleBot.start();
