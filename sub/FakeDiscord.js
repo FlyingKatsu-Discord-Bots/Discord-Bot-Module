3b2a4b3d8f292f49c3ae713009075602d29e4c0b
Flying Katsu <Warped2713@users.noreply.github.com>
2017-06-30 17:28:10 -0400

    FakeDiscord.Client emits message event with fake msg data when CLI receives 'msg <content>'

--- sub/FakeDiscord.js
+++ sub/FakeDiscord.js
@@ -1,10 +1,10 @@
 const util = require('util');
+const EventEmitter = require('events');
 
-class Client {
+class Client extends EventEmitter {
 
-  constructor (token) {
-    // TODO
-    this.token = token;
+  constructor () {
+    super();
   }
 
   login () {
@@ -13,6 +13,30 @@ class Client {
       process.stdin.setEncoding('utf8');
       process.stdin.on('data', (text) => {
         console.log(`Received data ${util.inspect(text)}`);
+        
+        if ( text.startsWith('msg ') ) {
+          
+          let msg = {
+            id: '0000000000000',
+            content: text.substr(4),
+            cleanContent: text.substr(4),
+            channel: { id: '1111111111111', type:'text'},
+            author: { id: '2222222222222', bot: false, username: 'FakeUser', tag:'1234' },
+            member: { id: '1111111111111',  nickname:'Member' },
+            guild: { id: '3333333333333' },
+            mentions: {
+              channels: new Map(),
+              everyone: false,
+              members: new Map(),
+              roles: new Map(),
+              users: new Map()
+            },
+            attachments: new Map()
+          };
+          
+          this.emit( 'message', msg );
+        }
+        
       });
       resolve();
     });
